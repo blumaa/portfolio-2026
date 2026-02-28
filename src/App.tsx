2080dea872e6c00704395ef8171df7229b43e225
aaron blum <blumaa@gmail.com>
2026-02-28 14:15:49 +0100

    fix error handling and tv route

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Projects } from './components/projects'
 import { Contact } from './components/contact'
 import { ThemeProvider } from './context/ThemeContext'
 import { TelevisionPage } from './pages/TelevisionPage'
+import { NotFound } from './pages/NotFound'
 
 function HomePage() {
   return (
@@ -29,6 +30,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/tv" element={<TelevisionPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
